a32b598dbbc84671785a37d4ee33afd319a665d3
LaraSudzi <96569964+LaraSudzi@users.noreply.github.com>
2022-08-28 22:39:31 +0300

    new2

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,15 @@ function action() {
   
 
   for (let i = 2; i < number; i++) {
-    console.log(number);
+    
 
     if (number % i == 0) {
-      console.log(false);
+      
       return resultPlace.innerHTML = 'Не простое число';
       
     }
   }
- console.log(true);
+ 
   return resultPlace.innerHTML = 'Простое число';
 
   
