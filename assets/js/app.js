dee10f53ea695d35c3b7d6faa53e67916ded4a09
LaraSudzi <96569964+LaraSudzi@users.noreply.github.com>
2022-08-29 19:57:14 +0300

    finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,24 @@
 function action() {
-  const arr = [];
-  let i = 1;
-  do {
-    arr.push(i);
-
-    //console.log(' arr.push(i);', i, arr);
-    for (let n = 0; n <= arr.length; n++) {
-      for (let j = 2; j < n; j++) {
-        console.log("i", i);
-        if (arr[n] % j == 0) {
-          console.log("false", arr.indexOf(i));
-          arr.splice(n, arr[n]);
-        }
-        console.log("true", arr);
-      }
-    }
-    i += 1;
-  } while (i < 100);
+  
+  let i = 3; 
+  let arr = []; 
+  do { 
+      
+      for (var n = 2; n < i; n++) { 
+          if (i % n === 0) { 
+           break; 
+          }  
+          
+         if (!arr.includes(i)) { 
+          arr.push(i); 
+          } 
+         
+      } 
+       
+        
+      i += 1; 
+   
+  } while (i < 1000); 
+   
   return (resultPlace.innerHTML = arr.join(', '));
 }
